3885820bd0b1fc2e47363cb95472184f88909b80
Emmiiez <emmiiez.m@gmail.com>
2024-06-10 23:19:21 +0700

    Update ex-2.js
    Update ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,14 @@
 //Exercise 2
 let lightBulbStatus = "On";
 
-// Start coding here.
+if (lightBulbStatus === "On") {
+    message = "Light bulb is On.";
+}else if(lightBulbStatus === "Off") {
+    message = "Light bulb is Off.";
+}else if(lightBulbStatus === "Broken") {
+    message = "Light blub is Broken";
+}else {
+    message = "Please choose the corrent input (On/Off/Broken)";
+}
+
+console.log(message);
